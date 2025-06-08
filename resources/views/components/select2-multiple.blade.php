932139aebb698b88d64044b8821fa4935739f670
Esteban Lillo <estebanlilloc@gmail.com>
2025-06-08 00:49:03 -0400

    fix select2 styles in dark mode

--- resources/views/components/select2-multiple.blade.php
+++ resources/views/components/select2-multiple.blade.php
@@ -1,12 +1,16 @@
 <div class="mb-4">
     @if($label)
-        <label for="{{ $id }}" class="block text-sm font-medium text-gray-900 mb-1">
+        <label for="{{ $id }}"
+               class="block text-sm font-medium text-gray-900 dark:text-gray-100 mb-1">
             {{ $label }}
         </label>
     @endif
 
-    <select id="{{ $id }}" name="{{ $name }}[]" multiple
-            class="select2 w-full shadow-xs {{ $class }}"
+    <select
+        id="{{ $id }}"
+        name="{{ $name }}[]"
+        multiple
+        class="select2 w-full rounded-md shadow-sm border-gray-300 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-100 focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 {{ $class }}"
     >
         @foreach($options as $option)
             <option value="{{ $option[$textField] }}"
@@ -16,7 +20,6 @@
             </option>
         @endforeach
     </select>
-
 
     @error($name)
     <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
